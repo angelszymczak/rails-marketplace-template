1d2ff3aec2543aa8f45349203e71f97ea6eb3a98
Angel Szymczak <juanangelszymczak@gmail.com>
2023-02-25 16:43:27 -0300

    test: add functional test

---
+++ spec/requests/products_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe 'Products' do
+  describe 'GET /products' do
+    it 'returns http OK' do
+      get '/products'
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe 'GET /products/:id' do
+    let(:product) { Product.create(title: 'sample', description: 'something', price: 100) }
+
+    it 'returns http OK' do
+      get '/products', params: { id: product.id }
+
+      expect(response).to have_http_status(:ok)
+    end
+  end
+end
