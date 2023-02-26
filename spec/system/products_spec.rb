d031247fafee416c33197a15f746414c3e4c783e
Angel Szymczak <juanangelszymczak@gmail.com>
2023-02-26 02:34:24 -0300

    test: check count products on the list

--- spec/system/products_spec.rb
+++ spec/system/products_spec.rb
@@ -2,10 +2,19 @@ require 'rails_helper'
 
 RSpec.describe 'Products' do
   describe 'visiting index' do
+    before do
+      Product.create([
+        { title: 'item1', description: 'new' },
+        { title: 'item2', description: 'old' },
+        { title: 'item3', description: 'used' },
+      ])
+    end
 
     it 'shows the site title' do
+      expect(Product.count).to eq(3)
       visit products_path
-      have_text 'Products:'
+      expect(page).to have_content('Products')
+      expect(page).to have_css('.product', count: 3)
     end
   end
 end
