78abc865c38dc2fd20da5ecb8d06d3b5e3650890
Angel Szymczak <juanangelszymczak@gmail.com>
2023-02-26 12:51:51 -0300

    test: check product detail page

--- spec/system/products_spec.rb
+++ spec/system/products_spec.rb
@@ -3,11 +3,14 @@ require 'rails_helper'
 RSpec.describe 'Products' do
   describe 'visiting index' do
     before do
-      Product.create([
-        { title: 'item1', description: 'new' },
-        { title: 'item2', description: 'old' },
-        { title: 'item3', description: 'used' },
-      ])
+      Product.create(
+        [
+
+          { title: 'item1', description: 'new' },
+          { title: 'item2', description: 'old' },
+          { title: 'item3', description: 'used' },
+        ]
+      )
     end
 
     it 'shows the site title' do
@@ -15,6 +18,15 @@ RSpec.describe 'Products' do
       visit products_path
       expect(page).to have_content('Products')
       expect(page).to have_css('.product', count: 3)
+    end
+  end
+
+  describe 'visiting a detailed item page' do
+    let!(:item) { Product.create(title: 'item', description: 'special') }
+
+    it 'shows the item title' do
+      visit product_path(item)
+      expect(page).to have_content(item.title)
     end
   end
 end
