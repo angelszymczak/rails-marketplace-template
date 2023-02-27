00974da779a2c4c5bc42f520ac14e228e50a24a3
Angel Szymczak <juanangelszymczak@gmail.com>
2023-02-27 19:49:47 -0300

    allow Guard to run specs in headless mode

--- spec/support/config/capybara.rb
+++ spec/support/config/capybara.rb
@@ -1,49 +1,27 @@
-# HOST = `cat /etc/resolv.conf | grep nameserver | awk '{ print $2 } | tail -n 1'`.strip
-# CHROMEDRIVER_URL = "https://#{HOST}:9515/"
-
 RSpec.configure do |config|
   config.before(:each, type: :system) do
-
-    # We should set it by default
-    driven_by :chrome
     Capybara.register_driver :chrome do |app|
       options = Selenium::WebDriver::Chrome::Options.new
 
       Capybara::Selenium::Driver.new(
         app,
+        options:,
         browser: :chrome,
-        options: options
       )
     end
 
-    # We should set it to run on ci/cd pipelines
-    # driven_by :headless_chrome
     Capybara.register_driver :headless_chrome do |app|
       options = Selenium::WebDriver::Chrome::Options.new
       options.add_argument('--headless')
 
       Capybara::Selenium::Driver.new(
         app,
+        options:,
         browser: :chrome,
-        options: options
       )
     end
 
-=begin
-    # May be, We should set it to run on ci/cd pipelines
-    # driven_by :selenium_remote_chrome
-    Capybara.register_driver :selenium_remote_chrome do |app|
-      options = Selenium::WebDriver::Chrome::Options.new
-      options.add_argument('--start-maximized')
-
-      Capybara::Selenium::Driver.new(
-        app,
-        browser: :remote,
-        url: CHROMEDRIVER_URL,
-        options: options
-      )
-    end
-=end
+    driven_by(ENV['RUNNER'] == 'CI' ? :headless_chrome : :chrome)
 
     Capybara.configure do |config|
       # Match what's set for URL options in test.rb
