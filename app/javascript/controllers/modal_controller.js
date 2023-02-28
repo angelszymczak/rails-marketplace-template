a231380e3da2f3b5974c3ca10f4389df919ee463
Angel Szymczak <juanangelszymczak@gmail.com>
2023-02-28 12:37:47 -0300

    add animation showing the add product form

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -3,7 +3,15 @@ import { Controller } from "@hotwired/stimulus";
 export default class extends Controller {
   static targets = ["sidebar"];
 
-  connect() {}
+  connect() {
+    // este metodo se ejecuta el html al que esa asociado es montado,
+    // en nuestro caso, cuando el turbo_frame traiga los resultados del backend
+    // y los imprima en el turbo_frame 'modal'
+    setTimeout(() => {
+      this.sidebarTarget.classList.remove("translate-x-full");
+      this.sidebarTarget.classList.add("translate-x-0");
+    }, 50);
+  }
 
   close() {
     this.sidebarTarget.classList.remove("translate-x-0");
@@ -13,6 +21,6 @@ export default class extends Controller {
     // en nuestro caso, todo lo que está dentro del turbo_frame
     setTimeout(() => {
       this.element.remove();
-    }, 300);
+    }, 250);
   }
 }
